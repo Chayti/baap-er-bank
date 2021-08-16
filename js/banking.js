e26a4ef96408475593ee843734ca11273150935c
Chayti Saha <chaytisaha98@gmail.com>
2021-08-17 01:58:40 +0600

    error handling

--- js/banking.js
+++ js/banking.js
@@ -14,10 +14,16 @@ function updateTotalField(id, amount) {
     Total.innerText = previousTotal + amount;
 }
 
-function updateBalance(amount, isAdd) {
+function getCurrentBalance() {
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
+    return previousBalanceTotal;
+}
+
+function updateBalance(amount, isAdd) {
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotal = getCurrentBalance();
     if (isAdd) balanceTotal.innerText = previousBalanceTotal + amount;
     else balanceTotal.innerText = previousBalanceTotal - amount;
 }
@@ -25,16 +31,24 @@ function updateBalance(amount, isAdd) {
 // deposit
 document.getElementById('deposit-btn').addEventListener('click', function () {
     const depositAmount = getInputValue('deposit-input');
-    updateTotalField('deposit-total', depositAmount); // get current deposit
-    updateBalance(depositAmount, true); // update balance
+    if (depositAmount > 0) { //error handling
+        updateTotalField('deposit-total', depositAmount); // get current deposit
+        updateBalance(depositAmount, true); // update balance
+    }
 })
 
 
 // withdraw
 document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawAmount = getInputValue('withdraw-input');
-    updateTotalField('withdraw-total', withdrawAmount); // get current withdraw
-    updateBalance(withdrawAmount, false); // update balance
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) { //error handling
+        updateTotalField('withdraw-total', withdrawAmount); // get current withdraw
+        updateBalance(withdrawAmount, false); // update balance
+    }
+    if (withdrawAmount > currentBalance) {
+        alert('You can\'t withdraw more than balance');
+    }
 })
 
 
